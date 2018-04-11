b1e926e9805ed5f97296c0c02811b98af656a6da
myownbag <1031772215@qq.com>
2018-04-11 20:46:20 +0800

    add InstrumentInputFregment View and Fragment

--- app/src/main/java/gc/dtu/weeg/dtugc/fregment/instrumentComSetFragment.java
+++ app/src/main/java/gc/dtu/weeg/dtugc/fregment/instrumentComSetFragment.java
@@ -6,9 +6,13 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 import gc.dtu.weeg.dtugc.R;
@@ -22,10 +26,16 @@ public class instrumentComSetFragment extends instrumentbaseFragment {
     Spinner mDatabit;
     Spinner mStopbit;
 
-    ArrayList<String> mBuadlist;
-    ArrayList<String> mParitylist;
-    ArrayList<String> mDatabitlist;
-    ArrayList<String> mStopbitlist;
+
+    ArrayAdapter<String> mBuadadpater;
+    ArrayAdapter<String> mParityadpater;
+    ArrayAdapter<String> mDatabitadpater;
+    ArrayAdapter<String> mStopbitadpater;
+
+    ArrayList<Map<String,String>> mBuadlist;
+    ArrayList<Map<String,String>> mParitylist;
+    ArrayList<Map<String,String>> mDatabitlist;
+    ArrayList<Map<String,String>> mStopbitlist;
     String[] mSettings;
     @Override
    public ArrayList<Map<String, String>> OnbutOKPress(byte[] sendbuf) {
@@ -49,23 +59,99 @@ public class instrumentComSetFragment extends instrumentbaseFragment {
         return mView;
     }
 
-    private void initview() {
+    private void initdata() {
+       Map<String,String> tmap=new HashMap<String,String>();
+        tmap.put("items","请选择");
+        tmap.put("value","请选择");
         mBuadlist=new ArrayList<>();
+        mParitylist=new ArrayList<>();
+        mDatabitlist=new ArrayList<>();
+        mStopbitlist=new ArrayList<>();
+        mBuadlist.add(tmap);
+        mParitylist .add(tmap);
+        mDatabitlist.add(tmap);
+        mStopbitlist.add(tmap);
         for(int i=0;i<mActivity.baseinfo.length;i++)
         {
             if(mActivity.baseinfo[i][0].equals("1998")==false)
             {
                 continue;
             }
-            else if(mActivity.baseinfo[i][1].equals("1")==false)
-            {
-                continue;
-            }
             else
             {
-                mBuadlist.add(mActivity.baseinfo[i][3]);
+                if(mActivity.baseinfo[i][1].equals("1"))
+                {
+                    Map<String,String> temp=new HashMap<>();
+                    temp.put("items",mActivity.baseinfo[i][3]);
+                    temp.put("value",mActivity.baseinfo[i][2]);
+                    mBuadlist.add(temp);
+                }
+                else if(mActivity.baseinfo[i][1].equals("2"))
+                {
+                    Map<String,String> temp=new HashMap<>();
+                    temp.put("items",mActivity.baseinfo[i][3]);
+                    temp.put("value",mActivity.baseinfo[i][2]);
+                    mParitylist.add(temp);
+                }
+                else if(mActivity.baseinfo[i][1].equals("3"))
+                {
+                    Map<String,String> temp=new HashMap<>();
+                    temp.put("items",mActivity.baseinfo[i][3]);
+                    temp.put("value",mActivity.baseinfo[i][2]);
+                    mDatabitlist.add(temp);
+                }
+                else if(mActivity.baseinfo[i][1].equals("4"))
+                {
+                    Map<String,String> temp=new HashMap<>();
+                    temp.put("items",mActivity.baseinfo[i][3]);
+                    temp.put("value",mActivity.baseinfo[i][2]);
+                    mStopbitlist.add(temp);
+                }
             }
+
         }
+    }
+
+    private ArrayAdapter<String> setSpinneradpater(Spinner spinner,ArrayList<Map<String,String>> arrayList )
+    {
+        //适配器
+        ArrayAdapter<String> arr_adapter;
+        String list[]=new String[arrayList.size()];
+        for(int i=0;i<arrayList.size();i++)
+        {
+            list[i]=arrayList.get(i).get("items");
+        }
+        arr_adapter = new ArrayAdapter<String>(mActivity, android.R.layout.simple_spinner_item, list);
+        //设置样式
+        arr_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //加载适配器
+        spinner.setAdapter(arr_adapter);
+        return  arr_adapter;
+    }
+    private void initview() {
+        initdata();
+        mBuadadpater=setSpinneradpater(mBuad,mBuadlist);
+        mParityadpater=setSpinneradpater(mParity,mParitylist);
+        mDatabitadpater=setSpinneradpater(mDatabit,mDatabitlist);
+        mStopbitadpater=setSpinneradpater(mStopbit,mStopbitlist);
+
+        mBuad.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                if(mBuadlist.get(0).get("items").equals("请选择"))
+//                {
+//                    mBuadlist.remove(0);
+//                    mBuadadpater=setSpinneradpater(mBuad,mBuadlist);
+//                    mBuadadpater.notifyDataSetChanged();
+//                    mBuad.setSelection(position-1,true);
+//                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     @Override
--- app/src/main/java/gc/dtu/weeg/dtugc/fregment/instrumentWorkModeSetFragment.java
+++ app/src/main/java/gc/dtu/weeg/dtugc/fregment/instrumentWorkModeSetFragment.java
@@ -6,14 +6,25 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 import gc.dtu.weeg.dtugc.R;
 
 public class instrumentWorkModeSetFragment extends instrumentbaseFragment {
     View mView;
+    Spinner mdevicestatus;
+    Spinner mdevicetype;
+    ArrayList<Map<String,String>> mdevicestatuslist;
+    ArrayList<Map<String,String>> mdevicetypelist;
+
+    ArrayAdapter<String> mdevicestatusad;
+    ArrayAdapter<String> mdevicetypead;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -22,10 +33,70 @@ public class instrumentWorkModeSetFragment extends instrumentbaseFragment {
             return mView;
         }
         mView=inflater.inflate(R.layout.instrument_workmode_setting_layout,null,false);
+        initview();
         return  mView;
     }
 
+    private void initview() {
+        initdata();
+        mdevicestatus=mView.findViewById(R.id.instrument_device_status_value);
+        mdevicetype=mView.findViewById(R.id.instrument_device_type_value);
+        setSpinneradpater(mdevicestatus,mdevicestatuslist);
+        setSpinneradpater(mdevicetype,mdevicetypelist);
+    }
 
+    private void initdata() {
+        Map<String,String> tmap=new HashMap<String,String>();
+        tmap.put("items","请选择");
+        tmap.put("value","请选择");
+        mdevicestatuslist=new ArrayList<>();
+        mdevicetypelist=new ArrayList<>();
+
+        mdevicestatuslist.add(tmap);
+        mdevicetypelist .add(tmap);
+
+        for(int i=0;i<mActivity.baseinfo.length;i++)
+        {
+            if(mActivity.baseinfo[i][0].equals("2000")==false)
+            {
+                continue;
+            }
+            else
+            {
+                if(mActivity.baseinfo[i][1].equals("1"))
+                {
+                    Map<String,String> temp=new HashMap<>();
+                    temp.put("items",mActivity.baseinfo[i][2]);
+                    temp.put("value",mActivity.baseinfo[i][3]);
+                    mdevicestatuslist.add(temp);
+                }
+                else if(mActivity.baseinfo[i][1].equals("2"))
+                {
+                    Map<String,String> temp=new HashMap<>();
+                    temp.put("items",mActivity.baseinfo[i][2]);
+                    temp.put("value",mActivity.baseinfo[i][3]);
+                    mdevicetypelist.add(temp);
+                }
+            }
+
+        }
+    }
+    private ArrayAdapter<String> setSpinneradpater(Spinner spinner,ArrayList<Map<String,String>> arrayList )
+    {
+        //适配器
+        ArrayAdapter<String> arr_adapter;
+        String list[]=new String[arrayList.size()];
+        for(int i=0;i<arrayList.size();i++)
+        {
+            list[i]=arrayList.get(i).get("items");
+        }
+        arr_adapter = new ArrayAdapter<String>(mActivity, android.R.layout.simple_spinner_item, list);
+        //设置样式
+        arr_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //加载适配器
+        spinner.setAdapter(arr_adapter);
+        return  arr_adapter;
+    }
     @Override
    public ArrayList<Map<String, String>> OnbutOKPress( byte[] sendbuf) {
         return null;
